52894cc351ba71e1a0fc989eccf418afe3039283
=Manuel <josemanuelwar@hotmail.es>
2025-08-31 18:06:39 -0600

    creando el curd de veterinaria

--- src/controllers/veterinaria/Veterinaria.controller.ts
+++ src/controllers/veterinaria/Veterinaria.controller.ts
@@ -1,31 +1,48 @@
 import { Request, Response } from "express";
 
 import VeterinariaService from "../../services/veterinaria/Veterinaria.Service";
+import { AuthRequest } from "../../middlewares/auth/authMiddleware";
 
-class VeterinariaController{
+class VeterinariaController {
+  private readonly service = new VeterinariaService();
 
-    private readonly service = new VeterinariaService();
-
-    public index = async (req:Request, res:Response)=>{
-
+  public index = async (req: AuthRequest, res: Response) => {
+    try {
+        const userId = req.user?.userId || 0;
+        const veterinaria = await this.service.getVeterinariaByUser(userId);
+        res.status(200).json(veterinaria);
+    } catch (error) { 
+        res.status(500).json({error:"error en el servidor"});
     }
+  };
 
-    public store= async(req:Request, res:Response)=>{
-        console.log(req.body);
+  public store = async (req: AuthRequest, res: Response) => {
+    try {
+        const userId = req.user?.userId;
+        const payload = req.body;
+        payload.userId = userId;
+        const veterinaria = await this.service.save(payload);
+        res.status(201).json(veterinaria);   
+    } catch (error) {
+        res.status(500).json({error:"error en el servidor"})
+    }
+  };
 
-        res.status(201).json({ message:"resa"});
+  public update = async (req: AuthRequest, res: Response) => {
+    try {
+        const UserId = req.user?.userId;
+        const veterinariaId = req.params.id;
+        const payload = req.body;
+        console.log("usuario ",UserId);
+        console.log("Veterinaria ", veterinariaId);
+        console.log("payload ", payload);
         
-
+         
+        
+    } catch (error) {
+        res.status(500).json({error:"error en el srvidor"})
     }
-
-    public edit = async(req:Request, res:Response)=>{
-
-    }
-
-    public update = async(req:Request, res:Response)=>{
-
-    }
-
+  };
 }
 
 export default new VeterinariaController();
--- src/routes/veterinaria.route.ts
+++ src/routes/veterinaria.route.ts
@@ -1,14 +1,33 @@
-import { Router } from 'express';
-import { validationMiddleware } from '../middlewares/validationMiddleware';
-import { authMiddleware } from '../middlewares/auth/authMiddleware';
-import { checkRoles } from '../middlewares/rolesMiddleware';
-import { CreatedVeterinariaDTO } from '../DTO/veterinaria/CreatedVeterinariaDTO';
-import VeterinariaController from '../controllers/veterinaria/Veterinaria.controller';
+import { Router } from "express";
+import { validationMiddleware } from "../middlewares/validationMiddleware";
+import { authMiddleware } from "../middlewares/auth/authMiddleware";
+import { checkRoles } from "../middlewares/rolesMiddleware";
+import { CreatedVeterinariaDTO } from "../DTO/veterinaria/CreatedVeterinariaDTO";
+import VeterinariaController from "../controllers/veterinaria/Veterinaria.controller";
 
 export default function veterinariaRouter(): Router {
-    const router = Router();
+  const router = Router();
 
-    router.post("/veterinaria/created",authMiddleware,checkRoles("admin"),validationMiddleware(CreatedVeterinariaDTO), VeterinariaController.store);
+  router.get(
+    "/veterinaria",
+    authMiddleware,
+    checkRoles("admin"),
+    VeterinariaController.index
+  );
+  router.post(
+    "/veterinaria/created",
+    authMiddleware,
+    checkRoles("admin"),
+    validationMiddleware(CreatedVeterinariaDTO),
+    VeterinariaController.store
+  );
+  router.patch(
+    "/veterinaria/update/:id",
+    authMiddleware,
+    checkRoles("admin"),
+    validationMiddleware(CreatedVeterinariaDTO),
+    VeterinariaController.update
+  );
 
-    return router;
+  return router;
 }
